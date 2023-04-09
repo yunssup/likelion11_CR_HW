9beb44f6f2d1e8311bd981fd2956f97e8f7fa6bb
jenny <88226911+kmjenny@users.noreply.github.com>
2023-04-09 14:34:37 +0900

    session04

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mainpage(request):
+    return render(request, 'main/mainpage.html')
+
+def secondpage(request):
+    return render(request, 'main/secondpage.html')
